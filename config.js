af8b1d2e219bb48f8ae1be5e127305cd56075a05
graciolli-f <graciolli.f@gmail.com>
2025-08-06 20:33:36 -0400

    vision modal + subscribe

---
+++ config.js
@@ -0,0 +1,10 @@
+// Configuration for MIDSPIRAL website
+const CONFIG = {
+    MAILERLITE: {
+        FORM_ID: '162030507390928363',
+        FORM_CONTAINER_ID: '29314228'
+    }
+};
+
+// Make config available globally
+window.CONFIG = CONFIG;
